116497ee4c0fe5f92f49d1639469566af40c9fef
tianhuizhou <tianhuizhou@users.noreply.github.com>
2020-11-28 18:22:01 -1000

    training model

--- chatbot_alpha.py
+++ chatbot_alpha.py
@@ -1,4 +1,5 @@
 import nltk
+import numpy as np
 from nltk.stem.porter import PorterStemmer
 
 # nltk.download('punkt')
@@ -17,9 +18,11 @@ def stem(word):
     return stemmer.stem(word.lower())
 
 
-words = ['organize', 'organization', 'organizing']
-stemmed_word = []
+def bag_of_words(tokenized_sentence, all_words):
+    tokenized_sentence = [stem(w) for w in tokenized_sentence]
+    bag = np.zeros(len(all_words), dtype=np.float32)
+    for idx, w in enumerate(all_words):
+        if w in tokenized_sentence:
+            bag[idx] = 1.0
+    return bag
 
-for w in words:
-    stemmed_word.append(stem(w))
-print(stemmed_word)
